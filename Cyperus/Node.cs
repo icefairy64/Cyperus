462910ef3c7902dc1490efb4499476eb3b95c434
icefairy64 <ishido.uu@gmail.com>
2015-03-07 00:05:13 +0200

    Initial commit

---
+++ Cyperus/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyperus
+{
+    abstract public class Node
+    {
+        public List<Socket> Inputs { get; protected set; }
+        public List<Socket> Outputs { get; protected set; }
+    }
+}
---
+++ Cyperus/Socket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyperus
+{
+    abstract public class Socket
+    {
+        public Type DataType { get; protected set; }
+        public List<Socket> Clients { get; protected set; }
+    }
+}
